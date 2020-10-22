9aef08178f6d2507e7e3f3e847a08b0b5d5699e4
Aditya Oke <okeaditya315@gmail.com>
2020-10-23 00:59:19 +0530

    adds equal sum and subset sum

---
+++ Dynamic_Programming/equal_sum.cpp
@@ -0,0 +1,93 @@
+// Partition problem is to determine whether a 
+// given set can be partitioned into two subsets such that the sum of elements in both subsets is the same. 
+
+// arr[] = {1, 5, 11, 5}
+// Output: true 
+// The array can be partitioned as {1, 5, 5} and {11}
+
+// arr[] = {1, 5, 3}
+// Output: false 
+// The array cannot be partitioned into equal sum sets.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int dp[100][100];
+
+bool is_subset_sum(int arr[], int sum, int n)
+{
+    if(dp[n][sum] != -1)
+    {
+        return dp[n][sum];
+    }
+
+    if(sum == 0)
+    {
+        return true;
+    }
+    if(n == 0)
+    {
+        return false;
+    }
+
+    else
+    {
+        if(arr[n-1] > sum)
+        {
+            dp[n][sum] = is_subset_sum(arr, sum, n-1);
+            return dp[n][sum];
+        }
+        else
+        {
+            dp[n][sum] = (is_subset_sum(arr, sum, n-1) || is_subset_sum(arr, sum-arr[n-1], n=1));
+            return dp[n][sum];
+        }
+        
+    }
+}
+
+bool equal_sum(int arr[], int n)
+{
+    int sum=0;
+    for(int i=0; i<n; i++)
+    {
+        sum += arr[i];
+    }
+    if(sum % 2 == 0)
+    {
+        return is_subset_sum(arr, sum/2, n);
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = { 3, 1, 5, 9, 12 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for(int i=0; i<100; i++)
+    {
+        for(int j=0;j<100; j++)
+        {
+            dp[i][j] = -1;
+        }
+    }
+    
+    // Function call
+    if (equal_sum(arr, n))
+    {
+        cout << "Can be divided into two subsets of equal sum";
+    }
+    else
+    {
+        cout << "Can not be divided into two subsets of equal sum";
+    }
+
+    return 0;
+}
+
+
--- Dynamic_Programming/subset_sum.cpp
+++ Dynamic_Programming/subset_sum.cpp
@@ -4,6 +4,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int dp[100][100];
+
+
 bool is_subeset_sum_rec(int set[], int sum, int n)
 {
     // Base Cases
@@ -27,6 +30,68 @@ bool is_subeset_sum_rec(int set[], int sum, int n)
     }
 }
 
+int is_subset_sum_mem(int set[], int sum, int n)
+{
+    if(dp[n][sum] != -1)
+    {
+        return dp[n][sum];
+    }
+    if(sum == 0)
+    {
+        return 1;
+    }
+    if(n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        if(set[n-1] > sum)
+        {
+            dp[n][sum] = is_subset_sum_mem(set, sum, n-1);
+            return dp[n][sum];
+        }
+        else
+        {
+            dp[n][sum] = is_subset_sum_mem(set, sum, n-1) || is_subset_sum_mem(set, sum-set[n-1], n-1);
+            return dp[n][sum];
+        }
+    }
+}
+
+int is_subset_sum_bu(int set[], int sum, int n)
+{
+    bool dp[n+1][sum+1];
+
+    // If sum is 0, then answer is true
+    for(int i=0; i<n; i++)
+    {
+        dp[i][0] = true;
+    }
+    // If sum is not 0 and set is empty, 
+    // then answer is false. It starts from 1.
+    for (int i = 1; i <= sum; i++) 
+    {
+        dp[0][i] = false;
+    }
+
+    for(int i=1; i<n+1; i++)
+    {
+        for(int j=1; j<sum+1; j++)
+        {
+            if(j < set[i - 1])
+            {
+                dp[i][j] = dp[i - 1][j]; 
+            }
+            else
+            {
+                dp[i][j] = dp[i-1][j] || dp[i-1][j - set[i - 1]];
+            }
+        }
+    }
+    return dp[n][sum];
+}
+
 int main(int argc, char const *argv[])
 {
     int set[] = {3, 34, 4, 12, 5, 2};
@@ -36,14 +101,40 @@ int main(int argc, char const *argv[])
 
     if(is_subeset_sum_rec(set, sum, n))
     {
-        cout<<"Yes";
+        cout<<"Yes"<<endl;
     }
     else
     {
-        cout<<"No";
+        cout<<"No"<<endl;
+    }
+
+    for(int i=0; i<100; i++)
+    {
+        for(int j=0;j<100; j++)
+        {
+            dp[i][j] = -1;
+        }
     }
     
+    if(is_subset_sum_mem(set, sum, n) == 1)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
 
+    if(is_subset_sum_bu(set, sum, n) == 1)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+
+    
     return 0;
 }
 
