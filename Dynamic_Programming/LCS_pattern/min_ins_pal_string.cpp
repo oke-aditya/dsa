ecca5c7648b986b1db3625eb77df646da311f5df
Aditya Oke <okeaditya315@gmail.com>
2020-11-29 18:44:03 +0530

    adds more lcs problems

---
+++ Dynamic_Programming/LCS_pattern/min_ins_pal_string.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Given a string str, the task is to find the minimum number of characters to be inserted to convert it to palindrome.
+// ab: Number of insertions required is 1 i.e. bab
+// aa: Number of insertions required is 0 i.e. aa
+// abcd: Number of insertions required is 3 i.e. dcbabcd
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
