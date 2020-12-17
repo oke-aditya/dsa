c46002ccea823782b6ff51e8591d8747a122601e
Aditya Oke <okeaditya315@gmail.com>
2020-12-17 16:04:11 +0530

    adds freq sort and k closes points

--- Data_Structures/Heap/freq_sort.cpp
+++ Data_Structures/Heap/freq_sort.cpp
@@ -17,13 +17,101 @@
 // Input: nums = [-1,1,-6,4,5,-6,1,4,1]
 // Output: [5,-1,4,4,-6,-6,1,1,1]
 
+// Solution: -
+// 1. Heap way
+// Prepare a hash map, with key and value pairs
+// Add one by one to the heap using values from map by frequency
+// We need just to add elements to heap, use min heap so that minimum value is at top.
+
+
 #include<bits/stdc++.h>
+#define pii pair<int, int>
+
 using namespace std;
+
+// To compare and return in correct order
+struct compare
+{
+    bool operator()(const pii &a, const pii &b)
+    {
+        if(a.first == b.second)
+        {
+            return a.second > a.first;
+        }
+        return a.first < b.first;
+    }
+};
+
+
+void print_v(vector <int> v)
+{
+    for(auto x : v)
+    {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+}
+
+vector <int> freq_sort_heap(vector <int> v)
+{
+    // Create a hashmap and store the frequencies
+    unordered_map <int, int> mp;
+    for(int i=0; i<v.size(); i++)
+    {
+        mp[v[i]] += 1;
+    }
+
+    // Print the map once to check
+    // for(auto i=mp.begin(); i!= mp.end(); i++)
+    // {
+    //     cout<<i->first<<" "<<i->second;
+    //     cout<<endl;
+    // }
+
+    // Push to min heap
+    priority_queue <pii, vector<pii>, greater<pii>> minh;
+
+    // Add the comparator declaration
+    // priority_queue <pii, vector<pii>, compare> minh2;
+
+    // We need pair to put a map into heap.
+
+    for(auto i=mp.begin(); i!= mp.end(); i++)
+    {
+        minh.push({i->second, i->first});
+    }
+
+    vector <int> res;
+
+    while(!minh.empty())
+    {
+        int freq = minh.top().first;
+        int ele = minh.top().second;
+
+        for(int i=1; i<=freq; i++)
+        {
+            res.push_back(ele);
+        }
+
+        minh.pop();
+    }
+
+    return res;
+
+}
 
 
 int main(int argc, char const *argv[])
 {
-    
+    vector <int> v {3,1,1,2,2,2};
+
+    auto res = freq_sort_heap(v);
+
+    print_v(res);
+
+    // auto res = freq_sort_md(v);
+
+
     return 0;
 }
 
