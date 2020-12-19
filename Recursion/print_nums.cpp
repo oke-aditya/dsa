519192d7737612c7522b28830e70c5844f671fc7
Aditya Oke <okeaditya315@gmail.com>
2020-12-20 01:52:56 +0530

    adds print nums

---
+++ Recursion/print_nums.cpp
@@ -0,0 +1,36 @@
+// print numbers 1 to n and n to 1 using recursion
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_to_n(int n)
+{
+    if (n == 1)
+    {
+        cout << n << " ";
+        return;
+    }
+    print_to_n(n - 1);
+    cout << n << " ";
+}
+
+void print_n_to(int n)
+{
+    if (n == 1)
+    {
+        cout << n << " ";
+        return;
+    }
+    cout << n << " ";
+    print_n_to(n - 1);
+}
+
+int main(int argc, char const *argv[])
+{
+    int n = 4;
+    print_to_n(n);
+    cout << endl;
+    print_n_to(n);
+    cout << endl;
+    return 0;
+}
