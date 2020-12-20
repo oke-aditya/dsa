126e235b84276c35fd6f41ee1fb8a35ee63b8c85
Aditya Oke <okeaditya315@gmail.com>
2020-12-20 14:11:28 +0530

    adds 2 more rec qs

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,38 @@
+// Find factorial of number using recursion
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact_rec(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n * fact_rec(n - 1));
+    }
+}
+
+int fact_itr(int n)
+{
+    int res = 1;
+    for(int i=1; i<=n; i++)
+    {
+        res *= i;
+    }
+    return res;
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int n = 4;
+    auto res = fact_rec(n);
+    auto res2 = fact_itr(n);
+    cout<<res<<endl;
+    cout<<res2<<endl;
+
+    return 0;
+}
---
+++ Recursion/tree_height.cpp
@@ -0,0 +1,61 @@
+// 1. If tree is empty then return 0
+// 2. Else
+//     (a) Get the max depth of left subtree recursively  i.e.,
+//         call maxDepth( tree->left-subtree)
+//     (a) Get the max depth of right subtree recursively  i.e.,
+//         call maxDepth( tree->right-subtree)
+//     (c) Get the max of max depths of left and right
+//         subtrees and add 1 to it for the current node.
+//         max_depth = max(max dept of left subtree,
+//                             max depth of right subtree)
+//                             + 1
+//     (d) Return max_depth
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *newNode(int data)
+{
+    Node *new_node = new Node();
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->data = data;
+
+    return new_node;
+}
+
+int height_rec(Node *root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        int l_depth = height_rec(root->left);
+        int r_depth = height_rec(root->right);
+
+        return max(l_depth, r_depth) + 1;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    Node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    cout << "Height of tree " << height_rec(root) << endl;
+
+    return 0;
+}
