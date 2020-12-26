509386234d519295b7af93a4c40075d3427eb5e3
Aditya Oke <okeaditya315@gmail.com>
2020-12-27 01:10:14 +0530

    adds nbit and josephus problem

--- Recursion/josephus.cpp
+++ Recursion/josephus.cpp
@@ -3,6 +3,8 @@
 // In each step, a certain number of people are skipped and the next person is executed.
 // The elimination proceeds around the circle (which is becoming smaller and smaller as the
 // executed people are removed), until only the last person remains, who is given freedom.
+
+
 // Given the total number of persons n and a
 // number k which indicates that k-1 persons are skipped and kth person
 // is killed in circle. The task is to choose
@@ -13,7 +15,33 @@
 // killed. Finally, the person at position 5 is killed.
 // So the person at position 3
 
-#include<bits/stdc++.h>
+// Solution: -
+// After the first person (kth from beginning) is killed, n-1 persons are left.
+// So we call josephus(n – 1, k) to get the position with n-1 persons.
+// But the position returned by josephus(n – 1, k)
+//  will consider the position starting from k%n + 1.
+// So, we must make adjustments to the position returned by josephus(n – 1, k).
+//   josephus(n, k) = (josephus(n - 1, k) + k-1) % n + 1
+//   josephus(1, k) = 1
+
+#include <bits/stdc++.h>
 using namespace std;
 
+int josephus(int n, int k)
+{
+    if (n == 1)
+    {
+        // Only one person, so he is left.
+        return 1;
+    }
+    // Else the problem has recurrence relation
+    return ((josephus(n - 1, k) + k - 1)) % n - 1;
+}
 
+int main(int argc, char const *argv[])
+{
+    int n = 4;
+    int k = 2;
+
+    return 0;
+}
