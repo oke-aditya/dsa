df1c44b44bfaf394be226afec4a82bb777774546
Aditya Oke <okeaditya315@gmail.com>
2020-12-27 01:14:46 +0530

    refactor repo remov *.py

--- Strings/reverse_words_dot.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-while(t>0):
-    s = input()
-    revstring = ''
-    l1 = s.split('.')
-    for i in l1:
-        revstring += i[::-1] + "."
-    print(revstring[:-1])
-    t -= 1
