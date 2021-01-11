691e010852936377e270a629cb0a74cd47ed3a8a
Aditya Oke <okeaditya315@gmail.com>
2021-01-11 16:27:04 +0530

    add more stuff

---
+++ Java/collections/queue_intro.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class queue_intro
+{
+    public static void main(String[] args)
+    {
+        Queue <Integer> q1 = new PriorityQueue<Integer> ();
+        q1.add(5);
+        q1.add(4);
+        q1.add(3);
+        q1.add(2);
+        q1.add(1);
+
+        System.out.println(q1);
+        
+        q1.remove(3);
+        q1.remove(2);
+
+        System.out.println(q1);
+
+        q1.add(2);
+        q1.add(3);
+
+        while(!q1.isEmpty())
+        {
+            System.out.println(q1.poll());
+        }
+
+        ArrayDeque <Integer> aq1 = new ArrayDeque <Integer>();
+
+        aq1.add(10);
+        aq1.add(20);
+        aq1.add(30);
+        aq1.add(40);
+
+        System.out.println(aq1);
+
+    }
+}
--- Java/collections/set_intro.java
+++ Java/collections/set_intro.java
@@ -5,7 +5,44 @@ public class set_intro
 {
     public static void main(String[] args)
     {
-        
+        HashSet <String> hs1 = new HashSet <String>();
+        hs1.add("Hello");
+        hs1.add("From");
+        hs1.add("Hello");
+        hs1.add("Set");
+
+        System.out.println(hs1);
+
+        LinkedHashSet <String> lsh1 = new LinkedHashSet <String> ();
+        lsh1.add("Hello");
+        lsh1.add("From");
+        lsh1.add("Linked");
+        lsh1.add("Hash");
+        lsh1.add("Set");
+
+        System.out.println(lsh1);
+
+        Object arr [] = hs1.toArray();
+        for(int j=0; j<arr.length; j++)
+        {
+            System.out.println(arr[j]);
+        }
+
+        SortedSet <String> ss1 = new TreeSet <String>();
+        ss1.add("Hello");
+        ss1.add("From");
+        ss1.add("Sorted");
+        ss1.add("Set");
+
+        System.out.println(ss1);
+
+        Object obj [] = ss1.toArray();
+        // System.out.println(obj);
+
+        for(int j=0; j<obj.length; j++)
+        {
+            System.out.println(obj[j]);
+        }
     }
     
 }
