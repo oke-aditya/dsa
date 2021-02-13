e56969ccccca82b13026de3a0c4b444fff0ceab4
Aditya Oke <okeaditya315@gmail.com>
2021-02-14 01:07:13 +0530

    add few more problems

--- Algorithms/Two_Pointer/three_sum.cpp
+++ Algorithms/Two_Pointer/three_sum.cpp
@@ -9,30 +9,69 @@
 // Input: nums = [0]
 // Output: []
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 vector<vector<int>> three_sum_opt(vector<int> v)
 {
     int n = v.size();
-    
+    vector<vector<int>> res;
+    sort(v.begin(), v.end());
 
+    // moves for a
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (i == 0 || (i > 0 && v[i] != v[i - 1]))
+        {
+            int left = i + 1, right = n - 1, sum = 0 - v[i];
+            while (left < right)
+            {
+                if (v[left] + v[right] == sum)
+                {
+                    vector<int> temp;
+                    temp.push_back(v[i]);
+                    temp.push_back(v[left]);
+                    temp.push_back(v[right]);
+                    res.push_back(temp);
+
+                    while (left < right && v[left] == v[left + 1])
+                    {
+                        left += 1;
+                    }
+                    while (left < right && v[right] == v[right - 1])
+                    {
+                        right -= 1;
+                    }
+                    left += 1;
+                    right -= 1;
+                }
+                else if (v[left] + v[right] < sum)
+                {
+                    left += 1;
+                }
+                else
+                {
+                    right -= 1;
+                }
+            }
+        }
+    }
+    return res;
 }
 
 vector<vector<int>> three_sum_brute(vector<int> v)
 {
     int n = v.size();
-    vector<vector <int>> res;
-    for(int i=0; i<n; i++)
+    vector<vector<int>> res;
+    for (int i = 0; i < n; i++)
     {
-        for(int j=i+1; j<n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            for(int k=j+1; k<n; k++)
+            for (int k = j + 1; k < n; k++)
             {
-                if((v[i] + v[j] + v[k]) == 0)
+                if ((v[i] + v[j] + v[k]) == 0)
                 {
-                    vector <int> temp;
+                    vector<int> temp;
                     temp.push_back(v[i]);
                     temp.push_back(v[j]);
                     temp.push_back(v[k]);
@@ -46,10 +85,9 @@ vector<vector<int>> three_sum_brute(vector<int> v)
 
 int main(int argc, char const *argv[])
 {
-    vector <int> v = {-1, 0, 1, 2, -1, -4};
+    vector<int> v = {-1, 0, 1, 2, -1, -4};
     auto res = three_sum_brute(v);
-
+    auto res2 = three_sum_opt(v);
 
     return 0;
 }
-
