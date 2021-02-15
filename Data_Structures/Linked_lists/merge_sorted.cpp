fd38cfbfb57ca1a6b1f7fdc28e76807862d55d24
Aditya Oke <okeaditya315@gmail.com>
2021-02-15 21:01:33 +0530

    adds more problems

---
+++ Data_Structures/Linked_lists/merge_sorted.cpp
@@ -0,0 +1,89 @@
+// Merge two sorted linked lists and return it as a sorted list.
+
+// The list should be made by splicing together the nodes of the first two lists.
+// For example if the first linked list a is 5->10->15 and the other 
+// linked list b is 2->3->20, then SortedMerge() 
+// should return a pointer to the head node of the merged list 2->3->5->10->15->20.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Method 1
+// Use a dummy node and extra linekd list to store result.
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+};
+
+void print_ln(Node *node)
+{
+    while(node != NULL)
+    {
+        cout<<node->data<<" ";
+        node = node->next;
+    }
+    cout<<endl;
+}
+
+void insert_head(Node **head_ref, int new_data)
+{
+    Node *new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+}
+
+Node* merge_lists(Node *node1, Node *node2)
+{
+    // Merge two linked lists, with help of dummy
+    // This is not inplace, we return a new linked list head
+    
+    // Dummy node. This is just a placeholder for new linked list.
+    Node *dummy = new Node();
+
+    // We will start appending from tail;
+    Node *tail;
+    // Point tail to dummy for now.
+    tail = dummy;
+
+    while(node1 != NULL || node2 != NULL)
+    {
+        if(node1->data >= node2->data)
+        {
+            
+        }
+        
+    }
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+    Node *head1 = NULL;
+    Node *head2 = NULL;
+
+    insert_head(&head1, 15);
+    insert_head(&head1, 10);
+    insert_head(&head1, 5);
+
+    cout<<endl<<"First list = ";
+    print_ln(head1);
+
+    insert_head(&head2, 20);
+    insert_head(&head2, 5);
+    insert_head(&head2, 3);
+
+    cout<<endl<<"Second list = ";
+    print_ln(head2);
+
+    auto merged_list = merge_lists(head1, head2);
+    print_ln(merge_lists);
+
+    return 0;
+}
+
--- Data_Structures/Linked_lists/reverse_list.cpp
+++ Data_Structures/Linked_lists/reverse_list.cpp
@@ -17,4 +17,62 @@
 // Input: 1->NULL
 // Output: 1->NULL 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+};
+
+void print_ln(Node *node)
+{
+    while (node != NULL)
+    {
+        cout<<node->data<<" ";
+        node = node->next;
+    }
+    cout<<endl;
+}
+
+void insert_head(Node **head_ref, int new_data)
+{
+    Node *new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+}
+
+Node* reverse_list(Node *head)
+{
+    Node *new_head = NULL;
+    while(head != NULL)
+    {
+        Node *temp = head->next;
+        head->next = new_head;
+        new_head = head;
+        head = temp;
+    }
+    return new_head;
+}
+
+int main(int argc, char const *argv[])
+{
+    Node *head = NULL;
+    insert_head(&head, 5);
+    insert_head(&head, 4);
+    insert_head(&head, 3);
+    insert_head(&head, 2);
+    insert_head(&head, 1);
+
+    print_ln(head);
+
+    auto new_head = reverse_list(head);
+
+    print_ln(new_head);
+
+    return 0;
+}
 
