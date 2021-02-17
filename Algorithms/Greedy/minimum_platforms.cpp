de36821283a4766076b9517ffbaa51b1c10b596b
Aditya Oke <okeaditya315@gmail.com>
2021-02-17 23:57:13 +0530

    add greedy problems

---
+++ Algorithms/Greedy/minimum_platforms.cpp
@@ -0,0 +1,31 @@
+// Minimum number of platforms required for the railway station so that no train is kept waiting.
+// Consider that all the trains arrive on the same day and leave on the same day.
+// Arrival and departure time can never be the same for a train but we can have arrival
+// time of one train equal to departure time of the other. At any given instance of time,
+// same platform can not be used for both departure of a train and arrival of another train. In such cases, we need different platforms,
+
+// Input: N = 6 
+// arr[] = {0900, 0940, 0950, 1100, 1500, 1800}
+// dep[] = {0910, 1200, 1120, 1130, 1900, 2000}
+// Output: 3
+// Explanation: 
+// Minimum 3 platforms are required to 
+// safely arrive and depart all trains.
+
+// Input: N = 3
+// arr[] = {0900, 1100, 1235}
+// dep[] = {1000, 1200, 1240}
+// Output: 1
+// Explanation: Only 1 platform is required to 
+// safely manage the arrival and departure 
+// of all trains.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
