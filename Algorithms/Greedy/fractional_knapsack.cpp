8fd92aac1e4c0893e9dbe845f41768919428f4ca
Aditya Oke <okeaditya315@gmail.com>
2021-03-02 00:48:09 +0530

    add greedy problems

--- Algorithms/Greedy/fractional_knapsack.cpp
+++ Algorithms/Greedy/fractional_knapsack.cpp
@@ -30,8 +30,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Item
+{
+    int value, weight;
+};
+
+bool comparator(Item a, Item b)
+{
+    auto a_vw = a.value / a.weight;
+    auto b_vw = b.value / b.weight;
+
+    return(a_vw > b_vw);
+
+}
+
+double fractional_knap(vector <Item> v, int W)
+{
+    sort(v.begin(), v.end(), comparator);
+
+    // Total current weight added.
+    int curr_w = 0;
+
+    // Finally value of knapsack.
+    double final_value = 0;
+
+    // Loop through all items;
+    for(int i=0; i < v.size(); i++)
+    {
+        // If we can add the item to knapsack.
+        if(curr_w + v[i].weight <= W)
+        {
+            curr_w += v[i].weight;
+            final_value += v[i].value;
+        }
+
+        // We can fill fraction.
+        else
+        {
+            // This is the remaining weight.
+            int remaining = W - curr_w;
+            // We need to add only fraction of value.
+            final_value += v[i].value * ((double) remaining / (double) v[i].weight);
+            
+            // Done the knapsack is filled.
+            break; 
+        }
+    }
+    return final_value;
+}
+
+
 int main(int argc, char const *argv[])
 {
+    int W = 50; //    Weight of knapsack
+    // Items <value, weight>
+    vector<Item> v = {{ 60, 10 }, { 100, 20 }, { 120, 30 }};
+
+    auto res = fractional_knap(v, W);
+
+    cout<<res<<endl;
     
     return 0;
 }
--- Algorithms/Greedy/job_sequencing.cpp
+++ Algorithms/Greedy/job_sequencing.cpp
@@ -30,16 +30,80 @@
 // maximum profit of 127 (100+27).
 
 // Solution: -
-// 
-// 
+// We can easily solve this using Greedy appraoch.
+// First we sort the jobs according to maximum profit possible to earn.
+// We would do the job on it's last deadline date possible.
+// Then we add the profits whichever jobs can be done.
+// We won't be doing some jobs. They are ones with least profits.
+// We can fill days before the deadline if it's not possible to do on last day.
 
 
 #include<bits/stdc++.h>
 using namespace std;
 
+// Data structure to store job details. Each job has an identifier,
+// a deadline, and profit associated with it.
+struct Job
+{
+    int taskID, deadline, profit;
+};
+
+bool comparator(Job a, Job b)
+{
+    return (a.profit > b.profit);
+}
+
+// Function to schedule jobs to maximize profit
+void scheduleJobs(vector<Job> jobs)  // no-ref, no-const
+{
+    // profit earnt.
+    int max_profit = 0;
+    
+    // An array to store used and unused info
+    vector <int> slot(15, -1);
+
+    // Sort the jobs according to maximum profits to earn.
+    sort(jobs.begin(), jobs.end(), comparator);
+
+    // Consider each job which are now sorted in order of profits
+    for(auto job: jobs)
+    {
+        // Search for next free slot and add it to that slot
+        for(int j=job.deadline-1; j>=0; j--)
+        {
+            if(slot[j] == -1)
+            {
+                slot[j] = job.taskID;
+                max_profit += job.profit;
+                // Allocate just once and break;
+                break;
+            }
+        }
+    }
+
+    // Print the scheduled jobs.
+    for(int i=0; i<slot.size(); i++)
+    {
+        // We have done it. Print the job id which is in the slot.
+        if(slot[i] != -1)
+        {
+            cout<<slot[i]<<" ";
+        }
+    }
+
+}
+
 int main(int argc, char const *argv[])
 {
-    
+    vector<Job> jobs =
+    {
+        { 1, 9, 15 }, { 2, 2, 2 }, { 3, 5, 18 }, { 4, 7, 1 }, { 5, 4, 25 },
+        { 6, 2, 20 }, { 7, 5, 8 }, { 8, 7, 10 }, { 9, 4, 12 }, { 10, 3, 5 }
+    };
+ 
+    // schedule jobs and calculate the maximum profit
+    scheduleJobs(jobs);
+    cout<<endl;
     return 0;
 }
 
