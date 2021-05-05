f1100e8fa6eee9991ff40b36c5d3767cfd5ae20c
Aditya Oke <okeaditya315@gmail.com>
2021-05-05 23:46:36 +0530

    new algos

---
+++ Data_Structures/Graph/minimum_spanning_tree_prims.cpp
@@ -0,0 +1,15 @@
+// Minimum Spanning Tree
+// 
+// 
+// Tree constructed from graph G, which has minimum weights
+// Also it spans the entire Graph G. It has n nodes and n -1 
+// edges.
+
+// Given a weighted, undirected and connected graph of V
+// vertices and E edges. The task is to find the sum
+// of weights of the edges of the Minimum Spanning Tree.
+
+
+// A
+
+
--- Data_Structures/Graph/shortest_path_undirect_djisktra.cpp
+++ Data_Structures/Graph/shortest_path_undirect_djisktra.cpp
@@ -13,12 +13,67 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector <int> shortest_path(int V, int src, vector<pair<int, int>> adj[])
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
+    vector<int> distances(V+1, INT_MAX);
 
+    // Source has 0 distance.
+    distances[src] = 0;
+
+    // source has 0 distance.
+    pq.push({0, src});
+
+    while(!pq.empty())
+    {
+        int dist = pq.top().first;
+        int prev = pq.top().second;
+
+        pq.pop();
+
+        // vector<pair<int, int>> :: iterator it;
+
+        for(auto it = adj[prev].begin(); it != adj[prev].end(); it++)
+        {
+            int next = it->first;
+            int next_dist = it->second;
+
+            if(distances[next] > distances[prev] + next_dist)
+            {
+                distances[next] = distances[prev] + next_dist;
+                pq.push({distances[next], next});
+            }
+        }
+    }
+    return distances;
+}
 
 int main(int argc, char const *argv[])
 {
-    
+    // Number of vertices and Edges.
+    int V, E;
+    cin >> V >> E;
+    vector<pair<int, int>> adj[V+1];
+    // Adjacenct list for all the vertices.
+
+    // Process each edge to build the graph.
+    for (int i = 0; i < E; i++)
+    {
+        int v1, v2, wt;
+        cin >> v1 >> v2 >> wt;
+        adj[v1].push_back({v2, wt});
+
+        // If directed graph this is not true.
+        adj[v2].push_back({v2, wt});
+    }
+
+    int src;
+
+    cin >> src;
+
+    auto res = shortest_path(V, src, adj);
+
     return 0;
 }
 
