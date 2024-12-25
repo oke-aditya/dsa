5437eead830de257ac04dd9630c8fccee2e62970
Oke Aditya <47158509+oke-aditya@users.noreply.github.com>
2024-12-25 23:52:53 +0530

    start studying backtracking

--- Algorithms/Backtracking/combination_sum1.cpp
+++ Algorithms/Backtracking/combination_sum1.cpp
@@ -1,124 +1,87 @@
-// Given an array of distinct integers nums and a target integer target,
-// return a list of all unique combinations of nums
-// where the chosen numbers sum to target. You may return the combinations in any order.
+// https://leetcode.com/problems/combination-sum/description/
 
-// The same number may be chosen from nums an
-// unlimited number of times. Two combinations are unique
-// if the frequency of at least one of the chosen numbers is different.
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
 
-// It is guaranteed that the number of unique combinations
-// that sum up to target is less than 150 combinations for the given input.
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
+// frequency
+// of at least one of the chosen numbers is different.
 
-// Input: nums = [2,3,6,7], target = 7
+// The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input. 
+
+// Example 1:
+
+// Input: candidates = [2,3,6,7], target = 7
 // Output: [[2,2,3],[7]]
-
 // Explanation:
-// 2 and 3 are nums, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+// 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
 // 7 is a candidate, and 7 = 7.
 // These are the only two combinations.
 
-// Input: nums = [2,3,5], target = 8
+// Example 2:
+
+// Input: candidates = [2,3,5], target = 8
 // Output: [[2,2,2,2],[2,3,3],[3,5]]
 
-// Input: nums = [2], target = 1
+// Example 3:
+
+// Input: candidates = [2], target = 1
 // Output: []
 
-// Input: nums = [1], target = 1
-// Output: [[1]]
 
-// Solution: -
-
-// Printing combinations / subsequence. Always involves recursion.
-// We have to recurse till we don't reach the target.
-// We have 2 choices for each element. Either we can pick it or we can avoid it.
-// We create a result array and a temp array, then we start backtracking.
-// backtrack till we reach end of the array.
-// if target is 0 we have found one solution in temp.
-// otherwise
-// we push the number which we have found.
-// Remember to pass result by reference.
-// Contrinue backtracking for num[i] - target
-// Do not increase i as we can take each element more than once.
-// If we are not recursing, the we should pop the element. So res.pop_back().
-
+// Note that we can pick multiple times the same number
+// We will need an extra temp array because we cannot do operations in place
+// We will backtrack the solution
 
 #include<bits/stdc++.h>
-
 using namespace std;
 
-void print_vv(vector <vector<int>> vv)
-{
-    for(auto v : vv)
+class Solution {
+public:
+
+    void backtrack(vector<int> &candidates, int start, int target, vector<int> &temp, vector<vector<int>> &res)
     {
-        cout<<"[";
-        for(auto ele: v)
+        // If target goes -ve. Discard the result, don't take it in res;
+        if(target < 0)
         {
-            cout<<ele<<" ";
+            return;
         }
-        cout<<"]";
-    }
-    cout<<endl;
-}
 
-// Pass the result by reference.
-void backtrack(vector<vector<int>> &res, vector<int> temp,
-               vector<int> nums, int target, int start)
-{
-    if(target < 0)
-    {
-        return;
-    }
-    // We have found one solution
-    else if(target == 0)
-    {
-        res.push_back(temp);
-    }
-    else
-    {
-        for(int i=start; i<nums.size(); i++)
+        // if we hit the target we have found a result
+        if (target == 0)
         {
-            // We cann take the element.
-            temp.push_back(nums[i]);
+            res.push_back(temp);
+            // we need to return since we don't need duplicates
+            return;
+        }
 
-            // we have to reduce the target.
-            // i -> Because we can re-use elements.
-            backtrack(res, temp, nums, target - nums[i], i);
+        // for all the choices from start coz of recursive calls
+        for(int i=start; i<candidates.size(); i++)
+        {
+            // if(i > start && (candidates[i] == candidates[i-1]))
+            // {
+            //     // no recerse
+            //     continue; 
+            // }
 
-            // Important.
-            // If we are not taking the element. We should remove the last element.
-            // temp.erase(temp.end()-1);
+            // backtrack by reducing target.
+            // take them in temp as possible result;
+            temp.push_back(candidates[i]);
+
+            // backtrack reducing target, we can take again
+            backtrack(candidates, i, target - candidates[i], temp, res);
+            // undo backtrack
             temp.pop_back();
         }
     }
-}
 
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        int start = 0;
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, start, target, temp, res);
 
-vector<vector<int>> print_combination(vector <int> nums, int target)
-{
-    vector<vector<int>> res;
-    vector<int> temp;
-    // If necessary we can sort
-    // sort(nums.begin(), nums.end());
-    backtrack(res, temp, nums, target, 0);
-    return res;
-
-}
-
-int main(int argc, char const *argv[])
-{
-    vector <int> candidates1 = {2, 3, 5};
-    int target1 = 7;
-
-    vector <int> candidates2 = {2, 3, 5};
-    int target2 = 8;
-
-    auto res1 = print_combination(candidates1, target1);
-    auto res2 = print_combination(candidates2, target2);
-
-    print_vv(res1);
-    print_vv(res2);
-
-    return 0;
-}
-
+        return res;
+    }
+};
 
---
+++ Algorithms/Backtracking/letter_case_permutation.cpp
@@ -0,0 +1,81 @@
+// https://leetcode.com/problems/letter-case-permutation/description/
+
+// Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
+
+// Return a list of all possible strings we could create. Return the output in any order.
+
+// Example 1:
+
+// Input: s = "a1b2"
+// Output: ["a1b2","a1B2","A1b2","A1B2"]
+
+// Example 2:
+
+// Input: s = "3z4"
+// Output: ["3z4","3Z4"]
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_vv(vector<string> vv)
+{
+    for(auto s: vv)
+    {
+        cout<<s<<", ";
+    }
+}
+
+void backtrack(string &s, int start, string &sol, vector<string> &res)
+{
+    // string size gives 4.
+    if(start == s.size())
+    {
+        res.push_back(sol);
+        return;
+    }
+
+    // notice we don't need for loop here, we are trying to use all combinations.
+    // We are not trying to combine 2 elements / together in array.
+    if(isalpha(s[start]))
+    {
+        // it's not OR case, we need both the combinations.
+        // In case interviewer twists the question asking to ToGGLe we can add if here.
+
+        sol += tolower(s[start]);
+        backtrack(s, start+1, sol, res);
+        sol.pop_back();
+
+        sol += toupper(s[start]);
+        backtrack(s, start+1, sol, res);
+        sol.pop_back();
+    }
+    else
+    {
+        sol += s[start];
+        backtrack(s, start+1, sol, res);
+        sol.pop_back();
+    }
+    
+}
+
+
+vector<string> letterCasePermutation(string s) {
+    vector<string> res;
+    int start = 0;
+    string sol = "";
+
+    backtrack(s, start, sol, res);
+
+    return res;
+}
+
+int main(int argc, char const *argv[])
+{
+    string s = "a1b2";
+    // cout<<s.size()<<endl;
+    auto res = letterCasePermutation(s);
+    print_vv(res);
+
+    return 0;
+}
