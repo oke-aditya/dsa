0af96ce2dac650c297e8aa6b94d5313f64c0fe56
Oke Aditya <47158509+oke-aditya@users.noreply.github.com>
2025-02-08 23:53:21 +0530

    add more hashing questions

--- Algorithms/Sliding_Window/fixed_size/count_ana.cpp
+++ Algorithms/Sliding_Window/fixed_size/count_ana.cpp
@@ -1,10 +1,10 @@
 // https://leetcode.com/problems/find-all-anagrams-in-a-string/
 
-// Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+// Given two strings s and p, return an array of all the start indices of p's anagrams in s.
+// You may return the answer in any order.
 
-// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
- 
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+// typically using all the original letters exactly once.
 
 // Example 1:
 
