2a9c40dc089fcb47e3606036433fb3e5148849bc
Oke Aditya <47158509+oke-aditya@users.noreply.github.com>
2025-03-27 22:27:23 +0530

    add more probs

---
+++ Data_Structures/Heap/medium/stock_price_fluctuation.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/stock-price-fluctuation/description/
+
+// You are given a stream of records about a particular stock. 
+// Each record contains a timestamp and the corresponding price of the stock at that timestamp.
+
+// Unfortunately due to the volatile nature of the stock market, the records do not come in order. 
+// Even worse, some records may be incorrect. Another record with the same timestamp may appear later 
+// in the stream correcting the price of the previous wrong record.
+
