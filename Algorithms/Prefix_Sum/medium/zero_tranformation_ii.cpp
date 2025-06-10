708e846ce5991b978cfaf9ea3b909cfd95f6e170
Oke Aditya <47158509+oke-aditya@users.noreply.github.com>
2025-06-11 00:08:11 +0530

    add difference arrays questions

--- Algorithms/Prefix_Sum/medium/zero_tranformation_ii.cpp
+++ Algorithms/Prefix_Sum/medium/zero_tranformation_ii.cpp
@@ -47,7 +47,69 @@
 // Solution:
 
 // Use same idea as difference array from 1.
-// Use binar search on queries to find best answer
+// Use binary search on queries to find best answer
+// same solution as zero array first, with added binary search
+
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+
+    bool is_possible(vector<int> nums, vector<vector<int>> queries, int k) {
+        int n = nums.size();
+        vector<int> prefix_sum(n+1, 0);
+
+        // we need to consider only k queries
+        for(int i=0; i<k; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int val = queries[i][2];
+
+            // you need to decrement the value
+            prefix_sum[l] += -val;
+            prefix_sum[r+1] -= -val;
+        }
+
+        for(int i=1; i<=n; i++) {
+            prefix_sum[i] += prefix_sum[i-1];
+        }
+        // it is possible if these prefix sum + nums makeit possible
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] != 0) {
+                if((nums[i] + prefix_sum[i]) > 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    
+    }
 
 
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int res = -1;
+        int n = queries.size();
+        int left = 0, right = n;
 
+        while(left <= right) {
+            int mid = left + (right - left ) / 2;
+
+            // try finding a better answer
+            // and store this ans
+            if(is_possible(nums, queries, mid)) {
+                res = mid;
+                right = mid - 1;
+            }
+
+            // not sure we can chop the search space one side? 
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return res;      
+
+    }
+};
